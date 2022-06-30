2c6c0234afeca00d4ae45352ad893d5cb1177ee4
A601-25 <oudontabetai0101@gmail.com>
2022-06-30 14:01:02 +0900

    u0630

--- v0630/Form1.cs
+++ v0630/Form1.cs
@@ -12,10 +12,45 @@ namespace v0630
 {
     public partial class Form1 : Form
     {
+        int vx = -5;
+        int vy = -10;
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if(label1.Left<0)
+            {
+                vx= Math.Abs(vx);
+            }
+            if(label1.Top<0)
+            {
+                vy = Math.Abs(vy);
+            }
+            if(label1.Right>ClientSize.Width)
+            {
+                vx = -Math.Abs(vx);
+            }
+            if (label1.Bottom > ClientSize.Height)
+            {
+                vy = -Math.Abs(vy);
+            }
+        }
     }
 }
